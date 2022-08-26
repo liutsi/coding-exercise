517411d9a7f2939c232e7e2786b15e2df1604fc1
bensom1989 <benson@brideli.com.au>
2022-08-26 16:30:54 +1000

    Initial commit

---
+++ src/test/java/com/coding/exercise/ExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
